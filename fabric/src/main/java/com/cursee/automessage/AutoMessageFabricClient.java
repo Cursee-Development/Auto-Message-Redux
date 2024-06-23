e6508406345b62aa4e48d0c453e66611d424a2de
jason13official <jason13official@gmail.com>
2024-06-22 20:59:24 -0400

    refactor and other shit

---
+++ fabric/src/main/java/com/cursee/automessage/AutoMessageFabricClient.java
@@ -0,0 +1,11 @@
+package com.cursee.automessage;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AutoMessageFabricClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
